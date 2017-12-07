d72969603549eb5f0972aa5af47f96976445ee68
Tarek <tarekakkawi89@gmail.com>
2017-12-07 00:06:26 +0000

    Graph alsmost done

--- Manufacturing/ManufacturingWPF/Stats/Stats.xaml.cs
+++ Manufacturing/ManufacturingWPF/Stats/Stats.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using LiveCharts.Wpf;
+using ManufacturingDB;
+using ManufacturingTest;
+using LiveCharts;
 
 namespace ManufacturingWPF
 {
@@ -20,9 +23,14 @@ namespace ManufacturingWPF
     /// </summary>
     public partial class Stats : Window
     {
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+
         public Stats()
         {
             InitializeComponent();
+            DisplayData();
         }
 
         /*Instructions of how to isntall new libs
@@ -37,7 +45,114 @@ namespace ManufacturingWPF
          *              i.  Type xmlns: followed by any letter/word (i.e xmlns:a) 
          *              ii. Insert namespace.
          *              iii.Once that's done go and type in your xmal ur code starting like this (<a:) */
+
+        private int SumOfNodes()
+        {
+            ManufacturingDataModel MDM = new ManufacturingDataModel();
+            Test t = new Test(MDM);
+
+            List<Hardware> SumOfHardware = t.GetHardware().FindAll(x => x.Nodes != 0);
+
+            int SumOfNodes = 0;
+
+            foreach (Hardware i in SumOfHardware )
+            {
+                SumOfNodes += i.Nodes;
+            }
+
+            return SumOfNodes;
+        }
+
+
+        private int SumOfRepeaters()
+        {
+            ManufacturingDataModel MDM = new ManufacturingDataModel();
+            Test t = new Test(MDM);
+
+            List<Hardware> SumOfHardware = t.GetHardware().FindAll(x => x.Repeaters != 0);
+
+            int SumOfRepeaters = 0;
+
+            foreach (Hardware i in SumOfHardware)
+            {
+                SumOfRepeaters += i.Repeaters;
+            }
+
+            return SumOfRepeaters;
+        }
+
+        private int SumOfHubs()
+        {
+            ManufacturingDataModel MDM = new ManufacturingDataModel();
+            Test t = new Test(MDM);
+
+            List<Hardware> SumOfHardware = t.GetHardware().FindAll(x => x.Hubs != 0);
+
+            int SumOfHubs= 0;
+
+            foreach (Hardware i in SumOfHardware)
+            {
+                SumOfHubs += i.Hubs;
+            }
+
+            return SumOfHubs;
+        }
+
+        private string Month()
+        {
+            DateTime now = DateTime.Now;
+
+            string month = DateTime.Now.ToString("MMMM");
+
+            return month;
+        }
+
+        private void DisplayData()
+        {
+
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                    {
+                          Title = "Nodes",
+                          Values = new ChartValues<int> { SumOfNodes() , 12313,123123 }
+                    },
+
+                 
+            };
+
+            SeriesCollection.Add
+            (
+                new ColumnSeries
+                {
+                    Title = "Repeaters",
+                    Values = new ChartValues<int> { SumOfRepeaters(), 12313, 123123 }
+                }
+            );
+
+            SeriesCollection.Add
+           (
+               new ColumnSeries
+               { 
+                   Title = "Hubs",
+                   Values = new ChartValues<int> { SumOfHubs() , 12313, 123123 }
+               }
+           );
+
+
             
-        
+            Labels = new[] {"leo", Month() , "Tarek" };
+            Formatter = value => value.ToString("N");
+
+            DataContext = this;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHardware sh = new ShowHardware();
+            sh.Show();
+            this.Close();
+        }
     }
 }
